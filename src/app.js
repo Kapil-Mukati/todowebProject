7be9d70954eaf7ef8b306649b2e09915a5868f54
Kapil-Mukati <98880544+Kapil-Mukati@users.noreply.github.com>
2024-03-15 10:07:20 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import userRouter from "../routes/user.js";
 import taskRouter from "../routes/task.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // configuration.....
 config({
@@ -15,6 +16,13 @@ const app = express();
 // Middelwares.....
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+);
 
 // using routes
 app.use("/app/v1/users", userRouter);
